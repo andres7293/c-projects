d9478b9695f64e3110936b5118037e21d3f81401
andres7293 <andres7293@gmail.com>
2017-06-12 21:18:41 +0100

    bcd2dec

---
+++ binary-op/bcd_to_dec.c
@@ -0,0 +1,35 @@
+#include "stdio.h"
+
+char getMSN(char number);
+char getLSN(char number);
+char toBCD(char msn,char lsn);
+
+void main()
+{
+  char number=25;
+  char msn=getMSN(number);
+  char lsn=getLSN(number);
+  printf("Number = %i\n\r",number);
+  printf("Most significant nibble = 0x%X\n\r",msn);
+  printf("Less significant nibble = 0x%X\n\r",lsn);
+  printf("BCD = dec %i \n\r",toBCD(msn,lsn));
+  printf("BCD = 0x%X \n\r",toBCD(msn,lsn));
+}
+
+char getMSN(char number)
+{
+  char msn = number/10;
+  return msn;
+}
+
+char getLSN(char number)
+{
+  char lsn = number % 10;
+  return lsn;
+}
+
+char toBCD(char msn,char lsn)
+{
+  char bcd = (msn << 4)|lsn;
+  return bcd;
+}
