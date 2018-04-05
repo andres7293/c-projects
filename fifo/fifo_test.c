e9ed09b5be6f97ac7ba9b8b83bae9910ef4519f4
Andres <andres7293@gmail.com>
2018-04-05 20:59:18 +0100

    More tests added

--- fifo/fifo_test.c
+++ fifo/fifo_test.c
@@ -10,14 +10,25 @@ int test_PopReturnValue (void);
 int test_PopContent (void);
 int test_PushMoreBytesThanBufferSizeReturnValue (void);
 int test_PushMoreBytesThanBufferSizeContent (void);
+int test_AvailableReturnValue (void);
+int test_NoBytesAvailableReturnValue (void);
+int test_PopMoreBytesThanPushedReturnValue (void);
+int test_PopMoreBytesThanPushedContent (void);
 
 int main (void) {
-    test_InitReturnValue();
-    test_PushReturnValue();
-    test_PushContent();
-    test_PopReturnValue();
-    test_PushMoreBytesThanBufferSizeReturnValue();
-    test_PushMoreBytesThanBufferSizeContent();
+    int num_tests = 10;
+    int success = 0;
+    success += test_InitReturnValue();
+    success += test_PushReturnValue();
+    success += test_PushContent();
+    success += test_PopReturnValue();
+    success += test_PushMoreBytesThanBufferSizeReturnValue();
+    success += test_PushMoreBytesThanBufferSizeContent();
+    success += test_AvailableReturnValue();
+    success += test_NoBytesAvailableReturnValue();
+    success += test_PopMoreBytesThanPushedReturnValue();
+    success += test_PopMoreBytesThanPushedContent();
+    printf("%i/%i test passed\n", success, num_tests);
 	return 0;
 }
 
@@ -111,6 +122,63 @@ int test_PushMoreBytesThanBufferSizeContent (void) {
     int ret = memcmp(buffer, msg, sizeof(msg));
     if (ret == 0) {
         printf("test_PushMoreBytesThanBufferSizeContent failed\n");
+        return 0;
+    }
+    return 1;
+}
+
+int test_AvailableReturnValue (void) {
+    fifo_t fifo;
+    uint8_t buffer[64];
+    fifo_init(&fifo, buffer, sizeof(buffer));
+    uint8_t msg[] = {"This is a test\n"};
+    fifo_push(&fifo, msg, sizeof(msg));
+    int ret = fifo_available(&fifo);
+    if (ret != 1) {
+        printf("test_AvailableReturnValue failed\n");
+        return 0;
+    }
+    return 1;
+}
+
+int test_NoBytesAvailableReturnValue (void) {
+    fifo_t fifo;
+    uint8_t buffer[64];
+    fifo_init(&fifo, buffer, sizeof(buffer));
+    uint8_t msg[] = {"This is a test\n"};
+    int ret = fifo_available(&fifo);
+    if (ret != 0) {
+        printf("test_NoBytesAvailableReturnValue failed\n");
+        return 0;
+    }
+    return 1;
+}
+
+int test_PopMoreBytesThanPushedReturnValue (void) {
+    fifo_t fifo;
+    uint8_t buffer[4];
+    fifo_init(&fifo, buffer, sizeof(buffer));
+    uint8_t msg[] = {"This is a test\n"};
+    fifo_push(&fifo, msg, sizeof(msg));
+    int ret = fifo_pop(&fifo, msg, sizeof(msg));
+    if (ret == sizeof(msg)) {
+        printf("test_PopMoreBytesThanPushedReturnValue failed\n");
+        return 0;
+    }
+    return 1;
+}
+
+int test_PopMoreBytesThanPushedContent (void) {
+    fifo_t fifo;
+    uint8_t buffer[4];
+    fifo_init(&fifo, buffer, sizeof(buffer));
+    uint8_t msg[] = {"This is a test\n"};
+    fifo_push(&fifo, msg, sizeof(msg));
+    memset(msg, 0, sizeof(msg));
+    fifo_pop(&fifo, msg, sizeof(msg));
+    int ret = memcmp(buffer, msg, sizeof(msg));
+    if (ret == 0) {
+        printf("test_PopMoreBytesThanPushedContent failed\n");
         return 0;
     }
     return 1;
